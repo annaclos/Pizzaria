1add75eee401f7ab9f878819545431895aa01d9c
Anna Santos <anna.clara16f@yahoo.com.br>
2025-06-23 12:04:15 -0300

    front aesthetic change

---
+++ meuarquivo.js
@@ -0,0 +1,19 @@
+form.addEventListener("submit", function(event) {
+  let valido = true;
+
+  if (senha.value !== confirmarSenha.value || senha.value.length < 6) {
+    erroSenha.style.display = "block";
+    valido = false;
+  } else {
+    erroSenha.style.display = "none";
+  }
+
+  if (!/^\d{10,11}$/.test(telefone.value)) {
+    erroTelefone.style.display = "block";
+    valido = false;
+  } else {
+    erroTelefone.style.display = "none";
+  }
+
+  if (!valido) event.preventDefault();
+});
