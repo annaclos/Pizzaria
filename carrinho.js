605fed4a2a4e1d7bd65f7070afbca6c9422834ab
Anna Santos <anna.clara16f@yahoo.com.br>
2025-07-14 16:08:28 -0300

    mudar o alert pra um sweetalert2 ao finalizar carrinho

--- carrinho.js
+++ carrinho.js
@@ -1,4 +1,13 @@
-  function finalizarCompra() {
-    alert("Pedido finalizado com sucesso!! Agradecemos a preferência!");
+function finalizarCompra() {
+  Swal.fire({
+    title: "Pedido finalizado com sucesso!",
+    text: "Agradecemos a preferência!",
+    icon: "success",
+    showConfirmButton: false,
+    timer: 3000,
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+  }).then(() => {
     window.location.href = 'carrinho.html';
-  }
+  });
+}
